b274b1842e69a859773e67661fdadba91673ebd2
Sergey Solomentsev <tracer.nokia@gmail.com>
2018-05-12 22:08:49 +0300

    Introduced new properties functionality.

--- lib/src/PipelineContext.ts
+++ lib/src/PipelineContext.ts
@@ -3,6 +3,15 @@ import { PipelineMessage } from "./PipelineMessage";
 import { MessageFilter } from "./MessageFilter";
 import { PipelineMessageCollection } from "./PipelineMessageCollection";
 
+export class ContextProperty {
+    constructor(
+        public name: string,
+        public value: any,
+    ) {
+
+    }
+}
+
 /**
  * Introduces possibility to keep context information
  * about the flow of the pipeline, like:
@@ -11,7 +20,48 @@ import { PipelineMessageCollection } from "./PipelineMessageCollection";
 export class PipelineContext {
     public IsAborted: boolean = false;
 
+    protected Properties: ContextProperty[] = [];
     protected Messages: PipelineMessage[] = new PipelineMessageCollection();
+
+    public GetPropertyValueOrUndefined<T>(name: string): T {
+        let property = this.Properties.find(x => x.name == name);
+
+        if (property)
+            return <T>property.value;
+
+        return undefined;
+    }
+
+    public GetPropertyValueOrDefault<T>(name: string, defaultValue: T): T {
+        let property = this.Properties.find(x => x.name == name);
+
+        if (property)
+            return <T>property.value;
+
+        return defaultValue;
+    }
+
+    public AddOrSkipPropertyIfExists<T>(name: string, value: T) {
+        let property = this.Properties.find(x => x.name == name);
+
+        if (property)
+            return;
+
+        return this.Properties.push(new ContextProperty(name, value));
+    }
+
+    public SetOrAddProperty<T>(name: string, value: T) {
+        this.UpdateOrAddProperty(name, value);
+    }
+
+    public UpdateOrAddProperty<T>(name: string, value: T) {
+        let property = this.Properties.find(x => x.name == name);
+
+        if (property)
+            property.value = value;
+
+        return this.Properties.push(new ContextProperty(name, value));
+    }
 
     public GetMessages(filter: MessageFilter): PipelineMessage[] {
         if (this.Messages && this.Messages.length > 0) {
---
+++ test/PipelineContextTests.ts
@@ -0,0 +1,46 @@
+import { PipelineContext } from "../lib";
+import { expect } from "chai";
+
+describe("Pipeline context can create a new context", () => {
+
+    const PROPERTY_NAME = { TEST: "testProperty" };
+
+    specify("The property of child context will be undefined by default", () => {
+        let context: PipelineContext = new PipelineContext();
+
+        expect(context.GetPropertyValueOrUndefined(PROPERTY_NAME.TEST)).to.be.undefined;
+    });
+
+    specify("Newly created property will have a value on setter", () => {
+        let context: PipelineContext = new PipelineContext();
+        let val = "expectation";
+        context.SetOrAddProperty(PROPERTY_NAME.TEST, val);
+
+        expect(context.GetPropertyValueOrUndefined(PROPERTY_NAME.TEST)).eq(val);
+    });
+
+    specify("Setting property with existing value will change the value", () => {
+        let context: PipelineContext = new PipelineContext();
+        let val = "expectation";
+        context.SetOrAddProperty(PROPERTY_NAME.TEST, val);
+        let newVal = "New value of expectation";
+        context.SetOrAddProperty(PROPERTY_NAME.TEST, newVal);
+
+        expect(context.GetPropertyValueOrUndefined(PROPERTY_NAME.TEST)).eq(newVal);
+    });
+
+    specify("[AddOrSkipIfExists] Addding property with existing key will not change the value", () => {
+        let context: PipelineContext = new PipelineContext();
+        let val = "expectation";
+        context.SetOrAddProperty(PROPERTY_NAME.TEST, val);
+        let newVal = "New value of expectation";
+        context.AddOrSkipPropertyIfExists(PROPERTY_NAME.TEST, newVal);
+
+        expect(context.GetPropertyValueOrUndefined(PROPERTY_NAME.TEST)).eq(val);
+    });
+
+    /*
+    context.CreateChild<>();
+    context.GetOption();
+    */
+});
